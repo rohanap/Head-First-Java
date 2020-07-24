0e413825e2a90ab5143e9b4fddc646e87d3ceb45
rohanap <36108401+rohanap@users.noreply.github.com>
2020-07-24 21:10:49 +0530

    Add files via upload

---
+++ chap10/FullMoons.java
@@ -0,0 +1,24 @@
+
+import java.io.*;
+import java.util.*;
+import static java.lang.System.out;
+
+public class FullMoons {
+
+    static int DAY_IM = 1000 * 60 * 60 * 24;
+
+    public static void main(String[] args) {
+        Calendar c = Calendar.getInstance();
+        c.set(2004, 1, 7, 15, 40);
+
+        long day1 = c.getTimeInMillis();
+
+        for (int x = 0; x < 205; x++) {
+            day1 += (DAY_IM * 29.52);
+            c.setTimeInMillis(day1);
+            out.println(String.format("full moon on %tc", c));
+        }
+
+    }
+}
+
